e5100c8997515c41fbae5fc20020f9d39482c075
stevenjlance <steven.j.lance@gmail.com>
2022-12-06 09:21:59 -0500

    comments added

--- 2022/Day6/part2.js
+++ 2022/Day6/part2.js
@@ -1,6 +1,6 @@
 const { input } = require('./input');
 const data = input[0]
-
+// Starting values
 let char1 = data[0];
 let char2 = data[1];
 let char3 = data[2];
@@ -16,19 +16,24 @@ let char12 = data[11];
 let char13 = data[12];
 let char14 = ""
 
+// Iterate through entire data
 for(let i = 13; i < data.length; i++){
     let found = false;
     char14 = data[i]
     const string = char1 + char2 + char3 + char4 +char5 + char6 + char7 + char8 + char9 + char10 + char11 + char12 + char13 + char14
+    // Iterate through current string
     for(let j = 0; j < string.length; j++){
+        // Current character
         const stringChar = string[j]
         // iterate through remainder of string
         for(let inner = j + 1; inner < string.length; inner++){
             if(stringChar === string[inner]){
+                // Break the loop if found
                 found = true;
                 break
             }
         }
+        // Break this loop if found in the inner loop
         if(found){
             break;
         }
