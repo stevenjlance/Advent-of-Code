78a37a942b62ff47b61a15ef0ee2834c058348ac
stevenjlance <steven.j.lance@gmail.com>
2022-12-08 17:44:32 -0500

    fixed edge case

--- 2022/Day8/part2.js
+++ 2022/Day8/part2.js
@@ -11,7 +11,7 @@ for(let i = 0; i < input.length; i++){
 const transposeArray = arr =>
   arr[0].map((_, c) => arr.map(row => row[c]));
 
-const part2 = input =>
+const part2 = (input) =>
   input
   .map(
     (line, y, grid, transposed = transposeArray(grid)) =>
@@ -24,41 +24,63 @@ const part2 = input =>
           ]
         let val = parseInt(line[x],10);
         let rightCount = 0;
+        let leftCount = 0
+        let bottomCount = 0;
+        let topCount = 0;
         for(let i = 0; i < arr[0].length; i++){
             if(val > parseInt(arr[0][i])) rightCount++;
+            else if(i !== 0){
+                rightCount++
+                break
+            }
             else break;
         }
-        let leftCount = 0
+        
         if(rightCount !== 0){
             for(let i = 0; i < arr[1].length; i++){
                 if(val > parseInt(arr[1][i])) leftCount++;
+                else if(i !== 0){
+                    leftCount++
+                    break
+                }
                 else break;
             }
         }
-        let bottomCount = 0;
+        
         if(rightCount !== 0 && leftCount !== 0){
             for(let i = 0; i < arr[2].length; i++){
                 if(val > parseInt(arr[2][i])) bottomCount++;
+                else if(i !== 0){
+                    bottomCount++
+                    break
+                }
                 else break;
             }
         }
-        let topCount = 0;
+        
         if(rightCount !== 0 && leftCount !== 0 & bottomCount !== 0){
             for(let i = 0; i < arr[3].length; i++){
                 if(val > parseInt(arr[3][i])) topCount++;
+                else if(i !== 0){
+                    topCount++
+                    break
+                }
                 else break;
             }
         }
-        let score = topCount * bottomCount * rightCount * leftCount;
-        scoreGrid[x][y] = score;
-        return arr;
+        if(rightCount !== 0 && leftCount !== 0 & bottomCount !== 0 && topCount !== 0){
+            let score = topCount * bottomCount * rightCount * leftCount;
+            scoreGrid[x][y] = score;
+        }
+        
+        return scoreGrid;
       })
   )
 part2(input)
+console.log(scoreGrid)
 let max = -1;
 scoreGrid.forEach(row => {
     row.forEach(score => {
-        console.log(score)
         if(score > max){
             max = score
         }
