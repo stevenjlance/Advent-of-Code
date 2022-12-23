6f2bc9dd539f95940c89761b8c0551c8cee8cf99
stevenjlance <steven.j.lance@gmail.com>
2022-12-23 12:12:29 -0500

    Day 10 solution

--- 2015/Day10/part1.js
+++ 2015/Day10/part1.js
@@ -1,18 +1,29 @@
 const input = "1113122113"
 let acc = input;
-// for(let i = 0; i < 1; i++){
-//     let obj = {};
-//     for(let j = 0; j < acc.length; j++){
-//         let num = acc[j]
-//         // number doesn't exist or it's eq
-//         if(obj[num] === undefined){
-//             obj[num] = 1;
-//             console.log(obj)
-//         }
-//         else {
-//             obj[num] += 1;
-//         }
-//     }
-//     acc = obj
-//     console.log(obj)
-// }
+let round = 1;
+// change to 41 for part1
+while(round < 51){
+    let val = acc[0];
+    let count = 1;
+    let temp = "";
+    if(acc.length === 1){
+        temp = val + count;
+    }
+    else {
+        for(let i = 1; i < acc.length; i++){
+            if(val === acc[i]){
+                count++;
+            }
+            else {
+                temp += count.toString() + val;
+                count = 1;
+                val = acc[i]
+            }
+        }
+        temp += count.toString() + val;
+    }
+    acc = temp;
+    
+    round++
+}
+console.log(acc.length)
