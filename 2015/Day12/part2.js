b4ef71cedbf303ab84f1df31d7a6f84bd66e9e92
stevenjlance <steven.j.lance@gmail.com>
2022-12-30 18:15:19 -0500

    fixed part2 red code

--- 2015/Day12/part2.js
+++ 2015/Day12/part2.js
@@ -7,19 +7,16 @@ function nestedLoop(obj) {
     function recurse(obj, current) {
         for (const key in obj) {
             let value = obj[key];
-            if(value === "red"){
-                break;
-            }
             if(value != undefined) {
                 if (value && typeof value === 'object') {
-                    let children = Object.values(value);
-                    if(children.includes("red") && !Array.isArray(value)) {
-                        console.log(value)
-                        break
-                    };
-                    recurse(value, key);
+                    let flag = true;
+                    if(!Array.isArray(value)){
+                        for(let key in value){
+                            if(value[key] === "red") flag = false;
+                        }
+                    }
+                    if(flag) recurse(value, key);
                 } else {
-                  	// Do your stuff here to var value
                     res[key] = value;
                     if(typeof res[key] === "number"){
                         total += res[key]
