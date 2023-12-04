615c5d74a5267125c4c4ce73e02a368b0bf35d2c
stevenjlance <steven.j.lance@gmail.com>
2023-12-04 11:12:26 -0500

    day 4 part 2 solution

---
+++ 2023/Day4/part1.js
@@ -0,0 +1,24 @@
+const { input } = require('./input');
+let totalPoints = 0;
+input.forEach(card => {
+    let cardPoints = 0;
+    // Array of structure [card #, numbers to test, winning numbers]
+    let testCard = card[0].trim().split(": ")
+    testCard.push(card[1].trim())
+    // Pull off candidate and winning numbers. Replace all double spaces with single spaces
+    let candidateNumbers = testCard[1].trim().replace(/\s+/g, ' ').split(" ").map(num => Number(num))
+    let winningNumbers = testCard[2].trim().replace(/\s+/g, ' ').split(" ").map(num => Number(num))
+    // Covert each string to a number
+
+    candidateNumbers.forEach(num => {
+        if(winningNumbers.includes(num)){
+            if(cardPoints === 0) cardPoints = 1;
+            else cardPoints *= 2;
+        }
+    })
+    // Add summed card points to the total points
+    totalPoints += cardPoints
+})
+
+// Part 1 Answer
+console.log(totalPoints);
---
+++ 2023/Day4/part2.js
@@ -0,0 +1,38 @@
+const { input } = require('./input');
+let totalScratchCards = 0;
+
+input.forEach((card, index) => {
+    let currentCardNumber = index + 1
+    
+    // Array of structure [card #, numbers to test, winning numbers, totalScratchCards]
+    let testCard = card[0].trim().split(": ")
+    testCard.push(card[1].trim())
+    // Initially have one of each card
+    testCard.push(1)
+    
+    input[index] = testCard
+})
+
+input.forEach((card, index) => {
+    // Pull off candidate and winning numbers. Replace all double spaces with single spaces
+    let candidateNumbers = card[1].trim().replace(/\s+/g, ' ').split(" ").map(num => Number(num))
+    let winningNumbers = card[2].trim().replace(/\s+/g, ' ').split(" ").map(num => Number(num))
+    // Sum up the number of winningNumbers
+    let totalWinning = 0;
+    candidateNumbers.forEach(num => {
+        if(winningNumbers.includes(num)) totalWinning++;
+    })
+    // Starting at 1 more than the currentCard increase their scratchCard count
+    let multiplier = input[index][3]
+    for(let i = index + 1; i < index + totalWinning + 1; i++){
+        // The input[index][3] tells you how many times this will run (e.g. if you have 2 card 2's and each had 3 winning numbers, then you need to increase 3, 4, and 5 by 2 rather than 1)
+        input[i][3] = input[i][3] + multiplier
+    }
+    // console.log(input)
+})
+
+input.forEach(card => {
+    totalScratchCards += card[3]
+})
+
+console.log(totalScratchCards)
